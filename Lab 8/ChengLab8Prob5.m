2e8393b22401d26c64ba42fad058eb05c25f145d
Esther Cheng <esther588@berkeley.edu>
2022-03-18 21:48:34 -0700

    read file and separate data in vars

--- Lab 8/ChengLab8Prob5.m
+++ Lab 8/ChengLab8Prob5.m
@@ -5,17 +5,27 @@
 
 clear
 
-%Open the file to real
+%Open the file to read
 filename = '/Users/esthercheng/desktop/ENGR-405/Lab 8/Data2.xlsx';
 fid = fopen(filename, 'r');
 
 if fid == -1
    disp('File open not successful')
 else
-   %Reading from File
-   [voltage, time] = readvars(fid);
+   %Import the columns as separate variables
+   [voltage, time] = readvars(filename);
+
+   %Convert voltage from mV to temperature
+   for i = 1:size(voltage)
+       voltage(i,:) = voltage(i,:)/60;
+   end
+
+   %Convert time from minutes to hours
+   for j = 1:size(time)
+       time(j,:) = time(j,:)/60;
+   end
    
-   %Closing the File
+   %Closing the file
    closeresult = fclose(fid);
    if closeresult == 0
       disp('File close successful')
