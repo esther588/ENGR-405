efb4ccfdcfd0816990f1dd7178a82745aee60777
Esther Cheng <esther588@berkeley.edu>
2022-03-18 22:40:03 -0700

    finished prob 5

--- Lab 8/ChengLab8Prob5.m
+++ Lab 8/ChengLab8Prob5.m
@@ -14,16 +14,37 @@ if fid == -1
 else
    %Import the columns as separate variables
    [voltage, time] = readvars(filename);
+   
+   %Find maximum and minimum voltage readings
+   minVolt = min(voltage);
+   maxVolt = max(voltage);
+
+   %Convert corresponding temperatures to Fahrenheit
+   minTemp = 10*(9/5)+32;
+   maxTemp = 30*(9/5)+32;
+
+   %Find slope and y-intercept
+   slope = (maxVolt-minVolt)/(maxTemp-minTemp);
+   yInt = minVolt-minTemp*slope;
 
    %Convert voltage from mV to temperature
    for i = 1:size(voltage)
-       voltage(i,:) = voltage(i,:)/60;
+       voltage(i,:) = (voltage(i,:)-yInt)/slope;
    end
 
    %Convert time from minutes to hours
    for j = 1:size(time)
        time(j,:) = time(j,:)/60;
    end
+
+   %Plot graph, set ranges and tick marks for x and y axis, and include title
+   %%
+   plot(time, voltage);
+   xticks(0:3:24);
+   yticks(30:10:100);
+   xlabel('Time (Hours)');
+   ylabel('Temperature (F)');
+   title('Temperature (F) vs. Time (Hours)');
    
    %Closing the file
    closeresult = fclose(fid);
