507bccde2c226a37b8ecaa62d2cb7ab340f2247d
Esther Cheng <esther588@berkeley.edu>
2022-03-18 20:38:02 -0700

    finished prob 4

---
+++ Lab 8/ChengLab8Prob4.m
@@ -0,0 +1,28 @@
+%This script determins the components of the Partial Fraction Expansion
+% EstherCheng
+% ENGR 405
+% ChengLab8Problem4
+
+clear
+
+%Initialize the numerator and denominator polynomials
+num = [2, 14, 50, 62];
+den = [1, 5, 11, 15];
+
+%Find the residues, poles, and direct term of partial fraction expansion
+[r, p, k] = residue(num, den);
+
+%Display results
+fprintf("Coefficients of the quotient:");
+disp(k);
+fprintf("Roots of the denominator polynomial:\n");
+disp(p);
+fprintf("Coefficients of the partial fraction expansion terms:\n");
+disp(r);
+
+%Recover and display coefficients of the numerator and denominator polynomials
+[num, den] = residue(r, p, k);
+fprintf("Coefficients of the numerator polynomial: ");
+disp(num);
+fprintf("Coefficients of the denominator polynomial: ");
+disp(den);
