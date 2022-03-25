6cbb2198a1a42eb4161ee823d9c931896f5f8664
Esther Cheng <esther588@berkeley.edu>
2022-03-25 15:16:40 -0700

    finished geoSer

--- Lab 9/geoSer.m
+++ Lab 9/geoSer.m
@@ -1,21 +1,24 @@
 function gs = geoSer(r, varargin)
 %geoSer receives a value for r and calculate the sum of the geometric
 %series. If a second argument is passed to the function, it is the value 
-% of n; otherwise, the function generates a random integer
+% of n; otherwise, a random integer is generated
 %Format of Call: geoSer(r, varagin)
 %Returns the sum of the geometric series
 
-%Check if varargin is empty or not
-if size(varargin) == 0
+%Check if number of input argument is 1
+if nargin == 1
     %Generates a random integer in the range from 5 to 30
     n = randi([5, 30]);
 else
-    n = varargin(1,1);
+    %Initializes n to first element of varargin
+    n = varargin(1);
 end
 
 gs = 1 + r;
 
-for i=2:n
+%Calculate the sum of the geometric series, starting at 2 and going
+%until n
+for i = 2:n
     gs = gs + r^i;
 end
 
