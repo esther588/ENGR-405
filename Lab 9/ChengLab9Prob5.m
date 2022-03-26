0a125b2a99e09195823e4095e9425f5f6b441d2a
Esther Cheng <esther588@berkeley.edu>
2022-03-26 13:21:45 -0700

    created anon functions and stored them in mat file

---
+++ Lab 9/ChengLab9Prob5.m
@@ -0,0 +1,24 @@
+%This script creates the given anonymous functions and saves them into a
+%mat file
+
+% EstherCheng
+% ENGR 405
+% ChengLab9Problem5
+
+clc
+clear
+
+%Create the anonymous functions
+d2r = @(x) (x.*pi)./180;
+r2d = @(x) (x*180)./pi;
+cis = @(x) exp(1j*x);
+cisd = @(x) cis(dr2(x));
+angled = @(x) r2d(angle(x));
+
+%Create mat file and saves all equations into it
+mat_file = matfile('equations.mat','Writable',true);
+save('equations.mat','d2r');
+save('equations.mat','r2d');
+save('equations.mat','cis');
+save('equations.mat','cisd');
+save('equations.mat','angled');
