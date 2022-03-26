d4f8ba5a6906bdd2438b819083d4ef4850940e1c
Esther Cheng <esther588@berkeley.edu>
2022-03-25 22:09:09 -0700

    if statement for base cases

--- Lab 9/fib.m
+++ Lab 9/fib.m
@@ -1,6 +1,11 @@
-function num = fib(n)
+function fibNum = fib(n)
 %fib is a recursive function that receives a value of n and finds the nth
 %Fibonacci number
 %Format of Call: fib(n)
 %Returns the value that is the nth Fibonacci number
+
+%Check if n is 0 or 1
+if n == 0 || n == 1
+    fibNum = n;
+end
 end
