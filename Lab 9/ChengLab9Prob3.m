7acb31a8fd0f378ae48f59ff575d51362e52472a
Esther Cheng <esther588@berkeley.edu>
2022-03-25 21:28:26 -0700

    continue pulse and create script that calls pulse

---
+++ Lab 9/ChengLab9Prob3.m
@@ -0,0 +1,6 @@
+%This script computes and generates labeled plots for the given 
+% functions using 401 points over the range −3 ≤ t ≤ 3
+
+% EstherCheng
+% ENGR 405
+% ChengLab9Problem3
--- Lab 9/pulse.m
+++ Lab 9/pulse.m
@@ -1,6 +1,15 @@
 function y = pulse(t)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%pulse receives a vector and finds all elements that are less than 
+% or equal to -0.5 and greater than or equal to 0.5, changing them to 1. 
+% For all other elements, they will be changed to 0
+%Format of Call: pulse(t)
+%Returns the vector y
+
+%Locate the indices where −0.5 ≤ t ≤0.5
+indices = find(t >= -0.5 & t <= 0.5);
+
+%Use the zeros and ones functions to create y
+y = zeros(1, length(t));
+y(indices) = 1;
+
 end
