a165e18c0f71d5aa729fbb60bd190fc69025802c
Esther Cheng <esther588@berkeley.edu>
2022-03-25 21:06:45 -0700

    add input and output arguments

--- Lab 9/pulse.m
+++ Lab 9/pulse.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = untitled2(inputArg1,inputArg2)
+function y = pulse(t)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
